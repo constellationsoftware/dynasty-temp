7f3a574dd8c503234f02b17d0158cfb6e289f82f
Front Office Media <bamurph@gmail.com>
2011-10-10 12:32:32 -0400

    rebuilt

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dynasty::Application.config.secret_token = 'ed3e2a53bd48e6467b83acf8aadba7f426c1b764921533a6e1f470b6521475a4a4797333678fb8a8d0acd9918164508a38d38070a0a6f28fe699c217e5c09c60'
