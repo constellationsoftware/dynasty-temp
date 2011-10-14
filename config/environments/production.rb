7f4fea28c9c67af9df7d8fa57faf5c3f020ac564
Front Office Media <bamurph@gmail.com>
2011-10-14 10:36:45 -0400

    production error testing

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -6,7 +6,7 @@ Dynasty::Application.configure do
 
   # Full error reports are disabled and caching is turned on
   config.consider_all_requests_local       = true # set to true for fake dev mode
-  config.action_controller.perform_caching = true
+  config.action_controller.perform_caching = false # set to false for fake dev mode
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
   config.serve_static_assets = false
