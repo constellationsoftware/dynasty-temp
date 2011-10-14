1fef9c6108de1e1bc4aa6c89ec9c5ff41854c851
Front Office Media <bamurph@gmail.com>
2011-10-14 11:27:25 -0400

    temporarily rempoved devise auth from user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,11 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable, :confirmable,
+#  devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :trackable, :validatable, :lastseenable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
+#  attr_accessible :email, :password, :password_confirmation, :remember_me
 
   has_many :picking_orders
   has_one :user_team
