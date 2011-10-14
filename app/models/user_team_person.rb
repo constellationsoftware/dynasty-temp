63d39efa753b152ae6af5ae394169bed2234d303
Front Office Media <bamurph@gmail.com>
2011-10-14 11:32:55 -0400

    temporarily rempoved devise auth from user

--- app/models/user_team_person.rb
+++ app/models/user_team_person.rb
@@ -2,5 +2,5 @@ class UserTeamPerson < ActiveRecord::Base
   belongs_to :user_team
   belongs_to :person
 
-  requires :association, :user_team, :person
+#  requires :association, :user_team, :person
 end
