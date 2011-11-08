e4c0338dfe568e3c9b60f8c8c1e18de5ceda6e3e
Ben Murphy <ben@frontofficemedia.com>
2011-11-08 16:18:00 -0500

    Checked output

--- app/models/draft_observer.rb
+++ app/models/draft_observer.rb
@@ -1,16 +1,12 @@
 class DraftObserver < ActiveRecord::Observer
-  def after_create(model)
-    i = 1
+  def after_save(model)
+    i = 0
     number_of_teams = model.league.teams.count
-    draft_id = model.id
-    league_id = model.league_id
-    round = 0
-
     model.number_of_rounds.times do
-        round += 1
-        model.league.teams.each(:order => "id ASC") do |team|
+        model.league.teams.each do |team|
+            i += 1
             @pick = Pick.new
-            @pick.draft_id = draft_id
+            @pick.draft_id = model.id
             @pick.team_id = team.id
             @pick.pick_order = i
             @pick.save!
@@ -20,17 +16,3 @@ class DraftObserver < ActiveRecord::Observer
 end
 
 
-
-if model.draft.rounds.count.odd?
-      position = 1
-    else
-      position = model.league.user_teams.count
-    end
-    model.league.user_teams.each do |user_team|
-      model.picking_orders.create(:user_team => user_team, :position => position)
-      if model.draft.rounds.count.odd?
-        position += 1
-      else
-        position -= 1
-      end
-    end
