93c777b2eccff32cf1afb659d55cb8e852f942ad
Ben Murphy <ben@frontofficemedia.com>
2011-11-14 12:42:24 -0500

    Edited AbstractPlayer class connection info. Port can't be in quotes.

--- app/models/abstract_player_data.rb
+++ app/models/abstract_player_data.rb
@@ -1,14 +1,11 @@
-require 'active_record'
-$config = YAML.load_file(Rails.root + 'config' + 'database.yml')
-
-	class AbstractPlayerData < ActiveRecord::Base
-		self.abstract_class = true
-	    establish_connection(
-	      :adapter  => "mysql2",
-	      :host     => "50.16.184.114",
-	      :username => "app1455111",
-	      :password => "eSwWReno",
-	      :database => "app1455111"
-	    )
-	end
+class AbstractPlayerData < ActiveRecord::Base
+	self.abstract_class = true
+	establish_connection(
+	  :adapter  => "mysql2",
+	  :host     => "50.16.184.114",
+	  :port     => 3365,
+	  :username => "app1455111",
+	  :password => "eSwWReno",
+	  :database => "app1455111"
+	)
 end
