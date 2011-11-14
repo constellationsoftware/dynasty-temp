d6de5e7ad9c80f4b18b17fe4340262baef12ecc1
Ben Murphy <ben@frontofficemedia.com>
2011-11-14 11:30:33 -0500

    setup external db connection for xml team database on xeround

--- app/models/abstract_player_data.rb
+++ app/models/abstract_player_data.rb
@@ -3,6 +3,12 @@ $config = YAML.load_file(Rails.root + 'config' + 'database.yml')
 
 class AbstractPlayerData < ActiveRecord::Base
 	self.abstract_class = true
-
-	establish_connection ENV['XEROUND_DATABASE_URL']
+    establish_connection(
+      :adapter  => "mysql2",
+      :host     => "50.16.184.114",
+      :username => "app1455111",
+      :password => "eSwWReno",
+      :database => "app1455111"
+    )
+    end
 end
