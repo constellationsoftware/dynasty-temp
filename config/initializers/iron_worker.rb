0cbdee80e50db6bd975754538eb608d297579e27
Ben Murphy <ben@frontofficemedia.com>
2011-12-31 04:59:02 -0500

    removed IW initializer

--- config/initializers/iron_worker.rb
+++
@@ -1,6 +0,0 @@
-IronWorker.configure do |config|
-  config.token = 'H-6n-pFsiR4RFiwJFPnhXW7E8WI'
-  config.project_id = '4eebc865066bce1a4e0007a0'
-  # Use the line below if you're using an ActiveRecord database
-  config.database = Rails.configuration.database_configuration[Rails.env]
-end
