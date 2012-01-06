fb152af1ac498d3c0d60edf928e904717f3d6a46
Ben Murphy <ben@frontofficemedia.com>
2012-01-05 22:39:33 -0500

    if a draft is finished, creates PlayerTeamRecords for all picks made.
    this needs changed to update continuously but this works for now.
    
    Signed-off-by: Ben Murphy <ben@frontofficemedia.com>

--- app/models/pick_observer.rb
+++ app/models/pick_observer.rb
@@ -1,27 +1,23 @@
 class PickObserver < ActiveRecord::Observer
   def before_save(pick)
-    # if a player was picked, update the timestamp
-    if !!pick.player
-      pick.picked_at = Time.now
-    end
+# if a player was picked, update the timestamp
+  if !!pick.player
+    pick.picked_at = Time.now
+  end
   end
 
   def after_update(pick)
-    finished_count = Pick.joins{draft}.where{draft.id.eq pick.draft.id}.maximum(:pick_order)
+  finished_count = Pick.joins{draft}.where{draft.id.eq pick.draft.id}.maximum(:pick_order)
 
     if pick.pick_order === finished_count
       pick.draft.status = :finished
       pick.draft.finished_at = Time.now
       pick.draft.current_pick_id = nil
       pick.draft.save
+      # TODO: Edit this to update after each pick is made not at end of draft.
+      PlayerTeamRecord.match_picks
     end
 
-   #ptr = PlayerTeamRecord.new
-   #ptr.current = TRUE
-   #ptr.player_id = pick.player_id
-   #ptr.details = "Drafted in round #{pick.round}"
-   #ptr.user_team_id = pick.team_id
-   #ptr.added_at = pick.picked_at
-   #ptr.save
+
   end
 end
--- app/models/player_team_record.rb
+++ app/models/player_team_record.rb
@@ -2,4 +2,18 @@ class PlayerTeamRecord < ActiveRecord::Base
   set_table_name 'dynasty_team_players'
   belongs_to :player
   belongs_to :user_team
+
+  ## TODO: Make this not suck.
+  def self.match_picks
+    picks = Pick.all
+    picks.each do |pick|
+      ptr = PlayerTeamRecord.new
+      ptr.current = TRUE
+      ptr.player_id = pick.player_id
+      ptr.details = "Drafted in round #{pick.round}"
+      ptr.user_team_id = pick.team_id
+      ptr.added_at = pick.picked_at
+      ptr.save
+    end
+  end
 end
