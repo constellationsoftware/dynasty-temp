74e1641f3a62653722b49a79470d43fa27b8f4c5
Ben Murphy <ben@frontofficemedia.com>
2012-01-17 13:39:11 -0500

    fix for nginx
    Signed-off-by: Ben Murphy <ben@frontofficemedia.com>

--- app/models/draft_observer.rb
+++ app/models/draft_observer.rb
@@ -6,7 +6,7 @@ class DraftObserver < ActiveRecord::Observer
         #This is not a well documented feature, for more info start here:
         # http://groups.google.com/group/heroku/browse_thread/thread/bcdcf4e99bd35108
         #and also http://rubydoc.info/github/heroku/heroku
-        Heroku::Client.new('ben@frontofficemedia.com', 'fom556').ps_restart('dynastyowner', :ps => 'worker.1')
+        #Heroku::Client.new('ben@frontofficemedia.com', 'fom556').ps_restart('dynastyowner', :ps => 'worker.1')
 
     end
 end
