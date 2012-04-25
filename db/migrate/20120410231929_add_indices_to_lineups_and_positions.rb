a4655f4c30e026bfa379b2f6d90f78004190692f
Ben Murphy <bamurph@gmail.com>
2012-04-25 11:10:02 -0400

    pre merge commit

--- db/migrate/20120410231929_add_indices_to_lineups_and_positions.rb
+++
@@ -1,9 +0,0 @@
-class AddIndicesToLineupsAndPositions < ActiveRecord::Migration
-    def change
-        remove_index :dynasty_lineups, :name => 'index_dynasty_lineups_on_all'
-        add_index :dynasty_lineups, [ :position_id, :flex ]
-        add_index :dynasty_lineups, :string
-
-        add_index :dynasty_positions, :flex_position_id
-    end
-end
--- db/migrate/20120418203139_add_draft_id_to_dynasty_leagues.rb
+++
@@ -1,6 +0,0 @@
-class AddDraftIdToDynastyLeagues < ActiveRecord::Migration
-    def change
-        add_column :dynasty_leagues, :draft_id, :integer
-        add_index :dynasty_leagues, :draft_id, :unique => true
-    end
-end
