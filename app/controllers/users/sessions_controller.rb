88b49d795b5ac56f91d8db2aa861f4d64d6901a5
Nick <nick@frontofficemedia.com>
2012-05-25 14:14:23 -0400

    - The home page now correctly switches based on user login state

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,7 +1,7 @@
 module Users
     class SessionsController < Devise::SessionsController
         def after_sign_in_path_for(resource)
-            return user_root_path
+            return root_path
         end
     end
 end
--- config/initializers/haml_coffee.rb
+++ config/initializers/haml_coffee.rb
@@ -1,3 +1,3 @@
 unless Rails.env.production?
-    HamlCoffeeAssets::HamlCoffeeTemplate.name_filter = lambda { |n| n.sub /^templates\//, '' }
+    #HamlCoffeeAssets::HamlCoffeeTemplate.name_filter = lambda { |n| n.sub /^templates\//, '' }
 end
