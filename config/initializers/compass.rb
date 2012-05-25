6e0c8020bf5ffe99f7ff86b56a812b1841710a8c
Ben Murphy <bamurph@gmail.com>
2012-05-24 20:05:50 -0400

    production mode cleanup
    Signed-off-by: Ben Murphy <bamurph@gmail.com>

--- config/initializers/compass.rb
+++ config/initializers/compass.rb
@@ -1,13 +1,13 @@
 
-$ext_path = nil
-Compass.configuration do |config|
-    config.project_type = :rails
-    config.project_path = Rails.root
-    config.http_path = '/'
-    environment = Compass::AppIntegration::Rails.env
-
-    $ext_path = File.join(config.project_path, 'vendor', 'assets', 'javascripts', 'extjs')
-    require File.join($ext_path, 'resources', 'themes', 'compass_init')
-end
+#$ext_path = nil
+#Compass.configuration do |config|
+#    config.project_type = :rails
+#    config.project_path = Rails.root
+#    config.http_path = '/'
+#    environment = Compass::AppIntegration::Rails.env
+#
+#    $ext_path = File.join(config.project_path, 'vendor', 'assets', 'javascripts', 'extjs')
+#    require File.join($ext_path, 'resources', 'themes', 'compass_init')
+#end
 
 
