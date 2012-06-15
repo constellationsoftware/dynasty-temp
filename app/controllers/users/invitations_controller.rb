36f1b16def2b86d9d21e0df5afd0f9a77e1deaa7
Nick <nick@frontofficemedia.com>
2012-06-15 15:08:26 -0400

    - Added more reasonable default picture I guess :p - Registering through invites *should* be a lot more bulletproof now. It'll need extensive testing. The fix was to remove the CIM profile before putting a fresh one in its place, since correcting credit card OR user data put a CIM profile in there that locked you out of amending it

--- app/controllers/users/invitations_controller.rb
+++ app/controllers/users/invitations_controller.rb
@@ -2,21 +2,21 @@ class Users::InvitationsController < Devise::InvitationsController
     skip_before_filter :authenticate_user!, :only => [ :edit, :update ]
 
     def edit
-        super
-        @user = resource
+        @user = super
         @user
     end
 
     def update
-        self.resource = resource_class.accept_invitation!(params[resource_name])
+        resource_params = params[resource_name]
+        self.resource = resource_class.find_or_initialize_with_error_by(:invitation_token, resource_params[:invitation_token])
+        resource.attributes = resource_params
+        puts resource.inspect
+        puts "resource validity: #{resource.valid?.inspect}"
+        ret = process_payment(resource)
+        puts "process payment return value: #{ret}"
+        self.resource = resource_class.accept_invitation! resource_params if resource.valid? && ret
 
-        Rails.logger.debug "resource validity: #{resource.valid?.inspect}"
-        if resource.errors.empty?
-            unless resource.valid? && process_payment(resource)
-                respond_with_navigational(resource){ render :edit }
-                return
-            end
-
+        if resource.errors.empty? && ret
             set_flash_message :notice, :updated
             sign_in(resource_name, resource)
             respond_with resource, :location => after_accept_path_for(resource)
@@ -49,7 +49,7 @@ class Users::InvitationsController < Devise::InvitationsController
             # Wrap Credit Card in Payment
             payment = { :credit_card => credit_card }
             puts 'PAYMENT:'
-            Rails.logger.debug payment
+            pp payment
             puts ''
 
             # Create Address
@@ -79,59 +79,105 @@ class Users::InvitationsController < Devise::InvitationsController
                 }
             }
 
+            # assume we're encountering this again because of incorrect customer information
+            # and delete the customer profile before recreating them
+            unless user.customer_profile_id.nil?
+                delete_profile_response = GATEWAY.delete_customer_profile :customer_profile_id => user.customer_profile_id
+                pp 'delete customer profile response:'
+                pp delete_profile_response
+
+                if delete_profile_response.success?
+                    user.customer_profile_id = nil
+                    user.save
+                else
+                    set_flash_message :error, 'payment.profile_delete.error'
+                    user.errors[:payment] << 'Something went wrong with deleting the user profile'
+                end
+            end
+
             # Call the authorize.net API to create the profile
             options = { :profile => profile }
+            #save the customer profile id
+            #make sure this is not overwritten
             puts 'OPTIONS:'
-            Rails.logger.debug options
+            pp options
             puts ''
 
             create_profile_response = GATEWAY.create_customer_profile(options)
             pp 'create customer profile response:'
             pp create_profile_response
-            Rails.logger.debug 'response.authorization (customer payment profile id):'
-            Rails.logger.debug create_profile_response.authorization
+            puts 'response.authorization (customer payment profile id):'
+            pp create_profile_response.authorization
             puts ''
 
-            #save the customer profile id
-            #make sure this is not overwritten
             if create_profile_response.success?
                 # save the returned customer payment profile ID
                 user.customer_profile_id = create_profile_response.authorization
+                user.save
+            else
+                set_flash_message :error, 'payment.profile_create.error'
+                user.errors[:payment] << 'Something went wrong with creating the user profile'
+                return false
+            end
+=begin
+                # attempt to update the customer profile with all new data
+                profile[:customer_profile_id] = user.customer_profile_id
+                options = { :profile => profile }
+                puts 'OPTIONS:'
+                pp options
+                puts ''
 
-                # NOW GET THE CUSTOMER PROFILE & CREATE THE TRANSACTION
-                profile_options = { :customer_profile_id => user.customer_profile_id }
-                profile_response = GATEWAY.get_customer_profile(profile_options)
+                update_profile_response = GATEWAY.update_customer_profile(options)
+                pp 'update customer profile response:'
+                pp update_profile_response
+                puts 'response.authorization (customer payment profile id):'
+                pp update_profile_response.authorization
+                puts ''
 
+                unless update_profile_response.success?
+                    set_flash_message :error, 'payment.profile_update.error'
+                    user.errors[:payment] << 'Something went wrong with updating the user profile'
+                    return false
+                end
+=end
+
+            # NOW GET THE CUSTOMER PROFILE & CREATE THE TRANSACTION
+            profile_options = { :customer_profile_id => user.customer_profile_id }
+            profile_response = GATEWAY.get_customer_profile(profile_options)
+            if profile_response.success?
                 # Get the customer payment profile id for transaction
                 @customer_payment_profile_id = profile_response.params['profile']['payment_profiles']['customer_payment_profile_id']
                 puts "customer_payment_profile_id: #{@customer_payment_profile_id}"
                 puts ''
-
-                # build the transaction object
-                # TODO: refactor this - get amount from params
-                #:amount => (@user.tier == 'legend' ? 500 : 250)
-                @transaction = {
-                    :type => :auth_capture,
-                    :amount => (user.tier == 'legend' ? '0.02' : '0.01'),
-                    :customer_profile_id => user.customer_profile_id,
-                    :customer_payment_profile_id => @customer_payment_profile_id
-                }
-
-                ## Perform the transaction
-                @transaction_response = GATEWAY.create_customer_profile_transaction(:transaction => @transaction)
-                puts 'transaction response:'
-                Rails.logger.debug @transaction_response
-                puts ''
-                if @transaction_response.success?
-                    user.build_team :name => "#{user.username.capitalize}'s Team"
-                    return true
-                else
-                    user.errors[:payment] << 'Something went wrong with the transaction'
-                    set_flash_message :error, 'payment.transaction.error'
-                end
             else
-                user.errors[:payment] << 'Something went wrong with creating the user profile'
-                set_flash_message :error, 'payment.profile.error'
+                set_flash_message :error, 'payment.profile_fetch.error'
+                user.errors[:payment] << 'Something went wrong with fetching the user profile'
+                return false
+            end
+
+            # build the transaction object
+            # TODO: refactor this - get amount from params
+            #:amount => (@user.tier == 'legend' ? 500 : 250)
+            @transaction = {
+                :type => :auth_capture,
+                :amount => (user.tier == 'legend' ? '0.02' : '0.01'),
+                :customer_profile_id => user.customer_profile_id,
+                :customer_payment_profile_id => @customer_payment_profile_id
+            }
+
+            ## Perform the transaction
+            @transaction_response = GATEWAY.create_customer_profile_transaction(:transaction => @transaction)
+            puts 'transaction response:'
+            pp @transaction_response
+            puts ''
+            if @transaction_response.success?
+                user.build_team :name => "#{user.username.capitalize}'s Team"
+                user.save
+                return true
+            else
+                set_flash_message :error, 'payment.transaction.error'
+                user.errors[:payment] << 'Something went wrong with processing the transaction'
+                return false
             end
             false
         end
--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -116,8 +116,26 @@ module Users
                     }
                 }
 
+                # assume we're encountering this again because of incorrect customer information
+                # and delete the customer profile before recreating them
+                unless user.customer_profile_id.nil?
+                    delete_profile_response = GATEWAY.delete_customer_profile :customer_profile_id => user.customer_profile_id
+                    pp 'delete customer profile response:'
+                    pp delete_profile_response
+
+                    if delete_profile_response.success?
+                        user.customer_profile_id = nil
+                        user.save
+                    else
+                        set_flash_message :error, 'payment.profile_delete.error'
+                        user.errors[:payment] << 'Something went wrong with deleting the user profile'
+                    end
+                end
+
                 # Call the authorize.net API to create the profile
                 options = { :profile => profile }
+                #save the customer profile id
+                #make sure this is not overwritten
                 puts 'OPTIONS:'
                 pp options
                 puts ''
@@ -129,46 +147,74 @@ module Users
                 pp create_profile_response.authorization
                 puts ''
 
-                #save the customer profile id
-                #make sure this is not overwritten
                 if create_profile_response.success?
                     # save the returned customer payment profile ID
                     user.customer_profile_id = create_profile_response.authorization
+                    user.save
+                else
+                    set_flash_message :error, 'payment.profile_create.error'
+                    user.errors[:payment] << 'Something went wrong with creating the user profile'
+                    return false
+                end
+    =begin
+                    # attempt to update the customer profile with all new data
+                    profile[:customer_profile_id] = user.customer_profile_id
+                    options = { :profile => profile }
+                    puts 'OPTIONS:'
+                    pp options
+                    puts ''
 
-                    # NOW GET THE CUSTOMER PROFILE & CREATE THE TRANSACTION
-                    profile_options = { :customer_profile_id => user.customer_profile_id }
-                    profile_response = GATEWAY.get_customer_profile(profile_options)
+                    update_profile_response = GATEWAY.update_customer_profile(options)
+                    pp 'update customer profile response:'
+                    pp update_profile_response
+                    puts 'response.authorization (customer payment profile id):'
+                    pp update_profile_response.authorization
+                    puts ''
 
+                    unless update_profile_response.success?
+                        set_flash_message :error, 'payment.profile_update.error'
+                        user.errors[:payment] << 'Something went wrong with updating the user profile'
+                        return false
+                    end
+    =end
+
+                # NOW GET THE CUSTOMER PROFILE & CREATE THE TRANSACTION
+                profile_options = { :customer_profile_id => user.customer_profile_id }
+                profile_response = GATEWAY.get_customer_profile(profile_options)
+                if profile_response.success?
                     # Get the customer payment profile id for transaction
                     @customer_payment_profile_id = profile_response.params['profile']['payment_profiles']['customer_payment_profile_id']
                     puts "customer_payment_profile_id: #{@customer_payment_profile_id}"
                     puts ''
-
-                    # build the transaction object
-                    # TODO: refactor this - get amount from params
-                    #:amount => (@user.tier == 'legend' ? 500 : 250)
-                    @transaction = {
-                        :type => :auth_capture,
-                        :amount => (user.tier == 'legend' ? '0.02' : '0.01'),
-                        :customer_profile_id => user.customer_profile_id,
-                        :customer_payment_profile_id => @customer_payment_profile_id
-                    }
-
-                    ## Perform the transaction
-                    @transaction_response = GATEWAY.create_customer_profile_transaction(:transaction => @transaction)
-                    puts 'transaction response:'
-                    pp @transaction_response
-                    puts ''
-                    if @transaction_response.success?
-                        user.build_team :name => "#{user.username.capitalize}'s Team"
-                        return true
-                    else
-                        user.errors[:payment] << 'Something went wrong with the transaction'
-                        set_flash_message :error, 'payment.transaction.error'
-                    end
                 else
-                    user.errors[:payment] << 'Something went wrong with creating the user profile'
-                    set_flash_message :error, 'payment.profile.error'
+                    set_flash_message :error, 'payment.profile_fetch.error'
+                    user.errors[:payment] << 'Something went wrong with fetching the user profile'
+                    return false
+                end
+
+                # build the transaction object
+                # TODO: refactor this - get amount from params
+                #:amount => (@user.tier == 'legend' ? 500 : 250)
+                @transaction = {
+                    :type => :auth_capture,
+                    :amount => (user.tier == 'legend' ? '0.02' : '0.01'),
+                    :customer_profile_id => user.customer_profile_id,
+                    :customer_payment_profile_id => @customer_payment_profile_id
+                }
+
+                ## Perform the transaction
+                @transaction_response = GATEWAY.create_customer_profile_transaction(:transaction => @transaction)
+                puts 'transaction response:'
+                pp @transaction_response
+                puts ''
+                if @transaction_response.success?
+                    user.build_team :name => "#{user.username.capitalize}'s Team"
+                    user.save
+                    return true
+                else
+                    set_flash_message :error, 'payment.transaction.error'
+                    user.errors[:payment] << 'Something went wrong with processing the transaction'
+                    return false
                 end
                 false
             end
