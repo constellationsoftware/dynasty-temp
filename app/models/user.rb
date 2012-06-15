34975b2f34cf2936e85958b0bce2e2c6296b87e4
Ben Murphy <bamurph@gmail.com>
2012-06-15 00:25:16 -0400

    Fixed invite bug
    One line = 2 hours of pain.
    
    Signed-off-by: Ben Murphy <bamurph@gmail.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -71,7 +71,7 @@ class User < ActiveRecord::Base
     has_many :events, :through => :event_subscriptions
     has_one :invitation, :class_name => 'User', :as => :invited_by
 
-    attr_accessible :roles, :email, :username, :login, :password, :password_confirmation, :remember_me, :last_seen, :id, :current_sign_in_at, :current_sign_in_ip, :phone, :area_code, :notifications, :first_name, :last_name, :tier, :address_attributes, :event_subscriptions_attributes, :address
+    attr_accessible :roles, :email, :username, :login, :password, :password_confirmation, :remember_me, :last_seen, :id, :current_sign_in_at, :current_sign_in_ip, :phone, :area_code, :notifications, :first_name, :last_name, :tier, :address_attributes, :event_subscriptions_attributes, :address, :invited_by
 
     accepts_nested_attributes_for :address
 
